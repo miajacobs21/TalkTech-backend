8d57cc95709761cad944d654017aff5e660f8f12
miajacobs21 <mjacobs4@live.maryville.edu>
2023-04-19 18:16:23 -0400

    update-notification docs

--- src/features/notifications/controllers/test/update-notification.test.ts
+++ src/features/notifications/controllers/test/update-notification.test.ts
@@ -1,17 +1,15 @@
 import { Request, Response } from 'express';
 import { Server } from 'socket.io';
 import { authUserPayload } from '@root/mocks/auth.mock';
-
+import * as notificationServer from '@socket/notification';
 import { notificationMockRequest, notificationMockResponse } from '@root/mocks/notification.mock';
 import { notificationQueue } from '@service/queues/notification.queue';
-import { Update } from '../update-notification';
-import { socketIONotificationObject } from '@socket/notification';
+import { Update } from '@notification/controllers/update-notification';
 
 jest.useFakeTimers();
 jest.mock('@service/queues/base.queue');
 
-// changed notificatonServer to notificationQueue //
-Object.defineProperties(notificationQueue, {
+Object.defineProperties(notificationServer, {
   socketIONotificationObject: {
     value: new Server(),
     writable: true
@@ -31,15 +29,11 @@ describe('Update', () => {
   it('should send correct json response', async () => {
     const req: Request = notificationMockRequest({}, authUserPayload, { notificationId: '12345' }) as Request;
     const res: Response = notificationMockResponse();
-    // added this following line after build failed 4.19.23
-    const socketIONotificationObject = {
-      emit: jest.fn()
-    };
-    jest.spyOn(socketIONotificationObject, 'emit');
+    jest.spyOn(notificationServer.socketIONotificationObject, 'emit');
     jest.spyOn(notificationQueue, 'addNotificationJob');
 
     await Update.prototype.notification(req, res);
-    expect(socketIONotificationObject.emit).toHaveBeenCalledWith('update notification', req.params.notificationId);
+    expect(notificationServer.socketIONotificationObject.emit).toHaveBeenCalledWith('update notification', req.params.notificationId);
     expect(notificationQueue.addNotificationJob).toHaveBeenCalledWith('updateNotification', { key: req.params.notificationId });
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith({
