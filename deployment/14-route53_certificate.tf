2ee98ede1d791e8fa046855ea0783d3104cd3829
miajacobs21 <mjacobs4@live.maryville.edu>
2023-05-05 15:56:39 -0400

    commented out aws features that already exisit, deployment 14,17,25

--- deployment/14-route53_certificate.tf
+++ deployment/14-route53_certificate.tf
@@ -12,14 +12,14 @@ resource "aws_acm_certificate" "dev_cert" {
   }
 }
 
-resource "aws_route53_record" "cert_validation_record" {
-  allow_overwrite = false
-  ttl             = 60
-  zone_id         = data.aws_route53_zone.main.zone_id
-  name            = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[0].resource_record_name
-  records         = [tolist(aws_acm_certificate.dev_cert.domain_validation_options)[0].resource_record_value]
-  type            = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[0].resource_record_type
-}
+# resource "aws_route53_record" "cert_validation_record" {
+#   allow_overwrite = false
+#   ttl             = 60
+#   zone_id         = data.aws_route53_zone.main.zone_id
+#   name            = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[0].resource_record_name
+#   records         = [tolist(aws_acm_certificate.dev_cert.domain_validation_options)[0].resource_record_value]
+#   type            = tolist(aws_acm_certificate.dev_cert.domain_validation_options)[0].resource_record_type
+# }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.dev_cert.arn
