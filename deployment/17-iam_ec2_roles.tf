1731ec041e5d5e3c40c424a2abb25081aa769325
miajacobs21 <mjacobs4@live.maryville.edu>
2023-05-05 13:56:03 -0400

    altered 17-iam_ec2_roles.tf file

--- deployment/17-iam_ec2_roles.tf
+++ deployment/17-iam_ec2_roles.tf
@@ -40,7 +40,7 @@ resource "aws_iam_role_policy" "ec2_iam_role_policy" {
 EOF
 }
 
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = var.ec2_instance_profile_name
-  role = aws_iam_role.ec2_iam_role.name
-}
+# resource "aws_iam_instance_profile" "ec2_instance_profile" {
+#   name = var.ec2_instance_profile_name
+#   role = aws_iam_role.ec2_iam_role.name
+# }
