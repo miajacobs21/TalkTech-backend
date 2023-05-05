c93a2f69a64eaaed49f14d2e967320ccd606248c
miajacobs21 <mjacobs4@live.maryville.edu>
2023-05-05 16:35:21 -0400

    profile2

--- deployment/17-iam_ec2_roles.tf
+++ deployment/17-iam_ec2_roles.tf
@@ -40,7 +40,7 @@ resource "aws_iam_role_policy" "ec2_iam_role_policy" {
 EOF
 }
 
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
+resource "aws_iam_instance_profile" "ec2_instance_profile2" {
   name = var.ec2_instance_profile_name
   role = aws_iam_role.ec2_iam_role.name
 }
